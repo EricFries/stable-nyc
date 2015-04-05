43e73839c35e4835869b015de6806e047f6ef619
EricFries <ericfries@gmail.com>
2015-04-05 17:03:46 -0400

    modify search algorithim to upcase street to match db entries.

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -8,8 +8,11 @@ class BuildingsController < ApplicationController
   end
 
   #Create a new building object based on the user input in order to render the view.  It is not saved to the database.
+  # :street in building_params needs to be upcase to match the database records (which are all upcased)
   def create
-    @building = Building.find_by(building_params)
+    search = building_params
+    search[:street] = search[:street].upcase
+    @building = Building.find_by(search)
 
     if !@building 
       @building = Building.new(building_params)
