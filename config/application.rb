cb8da40a4538eba8a65a42a6bf108e4f00ed854d
EricFries <ericfries@gmail.com>
2015-04-12 21:27:20 -0400

    change heroku setting

--- config/application.rb
+++ config/application.rb
@@ -33,7 +33,6 @@ module StableNyc
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     #try to fix heroku push failure with this:
-    config.assets.initialize_on_precompile = false
-
+  
   end
 end
